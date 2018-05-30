108f25215dc134d98a39d903d38eff8e444db615
Sunil Hirole <sunilhirole@gmail.com>
2018-05-30 19:06:32 +0530

    Setup app with database, routes and views

---
+++ app/routes.js
@@ -0,0 +1,44 @@
+//Routes
+
+
+module.exports = function (app, passport) {
+	
+	// Home page
+  app.get('/', function(req, res) {
+    res.render('index.ejs'); // load the index.ejs file
+  });
+
+  // Login page form
+  app.get('/login', function(req, res) {
+    // render the page and pass in any flash data if it exists
+    res.render('login.ejs', { message: req.flash('loginMessage') }); 
+  })
+  
+  //Signup form
+  app.get('/signup', function(req, res) {
+    // render the page and pass in any flash data if it exists
+    res.render('signup.ejs', { message: req.flash('signupMessage') });
+  });
+  
+  //Profile page
+  app.get('/profile', isLoggedIn, function(req, res) {
+    res.render('profile.ejs', {
+        user : req.user // get the user out of session and pass to template
+    });
+  });
+  
+  //Logout page
+  app.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('/');
+  });
+
+  // route middleware to make sure a user is logged in
+  function isLoggedIn(req, res, next) {
+    // if user is authenticated in the session, carry on 
+    if (req.isAuthenticated())
+        return next();
+    // if they aren't redirect them to the home page
+    res.redirect('/');
+  }
+}
---
+++ config/database.js
@@ -0,0 +1,4 @@
+// config/database.js
+module.exports = {
+  'url' : 'mongodb://sunil:test1234@ds139920.mlab.com:39920/node-passport'
+}
